beeb09592b1daf09bfdf2885923f03c9a31f4d94
KY imac <kengrany@princeton.edu>
2017-01-13 17:24:02 -0500

    sub plots

--- plot_PDF_0rh_diff.py
+++ plot_PDF_0rh_diff.py
@@ -8,6 +8,7 @@ Created on Sun Dec 13 18:00:08 2015
 from numpy import *
 from matplotlib.pyplot import *
 import sys
+from matplotlib import gridspec
 
 #import the target file
 import noNano_rh0 as target
@@ -56,15 +57,19 @@ matplotlib.rc('font', **font)
 
 # start plotting
 close("all")
-f, ax = subplots(figsize=(9,7))
+gs = gridspec.GridSpec(2, 1,
+                       height_ratios=[2,1]
+                       )
 
-[ax.plot(interm2_syn[i][0],np.array(interm2_syn[i][1])*norm_factor_wrt_first_curve[i], alpha=1,label='%s'%(name_of_the_plot[i])) for i in range (np.shape(data_files_1)[0])]
+# f, ax = subplots(nrows=2, ncols=1, sharex=True, figsize=(7,7))
+f = figure(figsize=(8,8))
+ax0 = subplot(gs[0])
 
-#difference plots
-[ax.plot(interm2_syn[i][0],(np.array(interm2_syn[i][1])*norm_factor_wrt_first_curve[i]-array(interm2_syn[0][1]))*5 + 6, '--', alpha=1,label='%s - %s'%(name_of_the_plot[i], name_of_the_plot[0])) for i in range (np.shape(data_files_1)[0])]
+[ax0.plot(interm2_syn[i][0],np.array(interm2_syn[i][1])*norm_factor_wrt_first_curve[i], alpha=1,label='%s'%(name_of_the_plot[i])) for i in range (np.shape(data_files_1)[0])]
 
-# [ax.plot(interm2_syn[i][0],(np.array(interm2_syn[i][1])-array(interm2_syn[0][1]))*5 + 10, '--', linewidth=1.5,alpha=1,label='%s - 0h no norm.'%(name_of_the_plot[i])) for i in range (np.shape(data_files_1)[0])]
 
+# f, ax = subplots(figsize=(9,7))
+# [ax.plot(interm2_syn[i][0],np.array(interm2_syn[i][1])*norm_factor_wrt_first_curve[i], alpha=1,label='%s'%(name_of_the_plot[i])) for i in range (np.shape(data_files_1)[0])]
 
 #add vertical lines at the local max
 
@@ -72,11 +77,11 @@ f, ax = subplots(figsize=(9,7))
 num_of_peak_showed = arange(9)
 num_of_peak_showed = append(num_of_peak_showed, [-3, -2, -1])
 
-lengthOftheLine = [0.92,0.8,0.85,0.8,0.8,0.8,0.8,0.8,0.8,0.8, 0.8, 0.8]
-[ax.axvline(pn.find_local_max(interm2_syn)[1][i],ymin=0,ymax=lengthOftheLine[i],color='k') for i in num_of_peak_showed]
+lengthOftheLine = [0.91, 0.3, 0.7, 0.4, 0.6, 0.8, 0.6, 0.55, 0.6, 0.8, 0.8, 0.8]
+[ax0.axvline(pn.find_local_max(interm2_syn)[1][i],ymin=0,ymax=lengthOftheLine[i],color='k') for i in num_of_peak_showed]
 
 #add names of the peaks
-position_y = [3.5, 1, 2.5 , 2.5, 3, 3.5, 2,2.7,3.5, 3, 3, 3]
+position_y = [3.5, -0.5, 2 , 0, 1.5, 2.7, 1.5, 1, 1.5]
 [text(pn.find_local_max(interm2_syn)[1][i],position_y[i], pn.name_of_the_peak(interm2_syn)[i],horizontalalignment='center',fontsize=14) for i in num_of_peak_showed]
 
 
@@ -85,13 +90,31 @@ locator_params(axis='x',tight=True,nbins=20)
 grid(axis='x')
 
 xlim (0.8,6)
-ylim (-4,9)
+
+# ylim (-4,9)
 #ax.axhline(y=0, color='k')
-xlabel(r'r ($\AA$)')
+
 ylabel('G(r)')
+legend(loc=0, fontsize ='small',ncol=1,columnspacing=0.5, labelspacing=0, frameon = True)
+
+'''difference plots'''
+ax1 = subplot(gs[1])
+mag_factor = 1
+
+[ax1.plot(interm2_syn[i][0],(np.array(interm2_syn[i][1])*norm_factor_wrt_first_curve[i]-array(interm2_syn[0][1])), '--',linewidth=1.5,alpha=1,label='%s - %s'%(name_of_the_plot[i], name_of_the_plot[0])) for i in range (np.shape(data_files_1)[0])]
+
+#difference plots
+# [ax.plot(interm2_syn[i][0],(np.array(interm2_syn[i][1])*norm_factor_wrt_first_curve[i]-array(interm2_syn[0][1]))*5 + 6, '--', alpha=1,label='%s - %s'%(name_of_the_plot[i], name_of_the_plot[0])) for i in range (np.shape(data_files_1)[0])]
+
+# [ax.plot(interm2_syn[i][0],(np.array(interm2_syn[i][1])-array(interm2_syn[0][1]))*5 + 10, '--', linewidth=1.5,alpha=1,label='%s - 0h no norm.'%(name_of_the_plot[i])) for i in range (np.shape(data_files_1)[0])]
+locator_params(axis='x',tight=True,nbins=20)
+grid(axis='x')
+xlabel(r'r ($\AA$)')
+xlim (0.8,6)
+ylim(-0.4, 0.6)
+# text(5,7.5,'x 5')
 legend(loc=0, fontsize ='small',ncol=2,columnspacing=0.5, labelspacing=0, frameon = True)
 
-text(5,7.5,'x 5')
 draw()
 f.show()
 
